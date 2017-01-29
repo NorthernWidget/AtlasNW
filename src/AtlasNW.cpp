62eb3c4e4742ac432045d52dcd808e237bb97219
Andy Wickert <andy@northernwidget.com>
2017-01-29 11:01:42 -0600

    Compiles

--- src/AtlasNW.cpp
+++ src/AtlasNW.cpp
@@ -32,7 +32,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include <AtlasNW.h>
 
-
 /////////////////////////////////////////////////////
 // STATIC MEMBERS OF CLASS, SO ACCESSIBLE ANYWHERE //
 /////////////////////////////////////////////////////
@@ -70,6 +69,12 @@ uint8_t softSerTX;
 // If I2C, save address
 uint8_t I2C_address;
 
+/////////////////////////
+// INSTANTIATE CLASSES //
+/////////////////////////
+
+//SoftwareSerial mySerial(softSerRX, softSerTX);
+
 /////////////////
 // CONSTRUCTOR //
 /////////////////
@@ -84,25 +89,25 @@ AtlasNW::AtlasNW(char* _measurement_type, char* _communications_type, uint8_t UA
   Serial.begin(38400); // hard-coded for now
   if(strcmp(_measurement_type, "conductivity") == 0){
     measurement_type = conductivity;
-  };
+  }
   else if(strcmp(_measurement_type, "pH") == 0){
     measurement_type = conductivity;
-  };
+  }
   else if(strcmp(_measurement_type, "ORP") == 0){
     measurement_type = conductivity;
-  };
+  }
   else if(strcmp(_measurement_type, "DO") == 0){
     measurement_type = conductivity;
-  };
+  }
   else if(strcmp(_measurement_type, "flow") == 0){
     measurement_type = conductivity;
-  };
+  }
   else if(strcmp(_measurement_type, "temperature") == 0){
     measurement_type = conductivity;
-  };
+  }
   else if(strcmp(_measurement_type, "color") == 0){
     measurement_type = conductivity;
-  };
+  }
   else{
     Serial.println("No valid sensor chosen. Halting.");
     while(1){};
@@ -112,25 +117,26 @@ AtlasNW::AtlasNW(char* _measurement_type, char* _communications_type, uint8_t UA
   if(strcmp(_communications_type, "UART") == 0){
     communications_type = use_UART;
     //Atlas_UART comms;
-  };
+  }
   else if(strcmp(_measurement_type, "SoftSerial") == 0){
     communications_type = use_SoftSerial;
-    SoftwareSerial mySerial (softSerRX, softSerTX);
-    //SoftSerial comms;
+    SoftwareSerial mySerial(softSerRX, softSerTX);
+    //SoftSerial comms; 
     
-  };
+  }
   else if(strcmp(_measurement_type, "I2C") == 0){
     communications_type = use_I2C;
     //Atlas_SoftSerial comms;
     Wire.begin(); // Concerns about loading this more than once in different
                   // libraries. See:
                   // https://github.com/esp8266/Arduino/issues/2607
-  };
+  }
   else{
     Serial.println("No valid measurement type chosen. Halting.");
-    while(1){};
+    while(1){}
     // Possibly add warning LED here
   }
+  //mySerial(softSerRX, softSerTX);
 }
 
 // Software serial: char* char* uint8_t uint8_t OPTuint32_t
@@ -139,55 +145,57 @@ AtlasNW::AtlasNW(char* _measurement_type, char* _communications_type, uint8_t _s
   Serial.begin(38400); // hard-coded for now
   if(strcmp(_measurement_type, "conductivity") == 0){
     measurement_type = conductivity;
-  };
+  }
   else if(strcmp(_measurement_type, "pH") == 0){
     measurement_type = conductivity;
-  };
+  }
   else if(strcmp(_measurement_type, "ORP") == 0){
     measurement_type = conductivity;
-  };
+  }
   else if(strcmp(_measurement_type, "DO") == 0){
     measurement_type = conductivity;
-  };
+  }
   else if(strcmp(_measurement_type, "flow") == 0){
     measurement_type = conductivity;
-  };
+  }
   else if(strcmp(_measurement_type, "temperature") == 0){
     measurement_type = conductivity;
-  };
+  }
   else if(strcmp(_measurement_type, "color") == 0){
     measurement_type = conductivity;
-  };
+  }
   else{
     Serial.println("No valid sensor chosen. Halting.");
-    while(1){};
+    while(1){}
     // Possibly add warning LED here
   }
   
   if(strcmp(_communications_type, "UART") == 0){
     communications_type = use_UART;
     //Atlas_UART comms;
-  };
+  }
   else if(strcmp(_measurement_type, "SoftSerial") == 0){
     communications_type = use_SoftSerial;
     softSerRX = _softSerRX;
     softSerTX = _softSerTX;
-    SoftwareSerial mySerial (softSerRX, softSerTX);
-    //SoftSerial comms;
-    
-  };
+    SoftwareSerial mySerial(softSerRX, softSerTX);
+    //SoftSerial comms; 
+  }
   else if(strcmp(_measurement_type, "I2C") == 0){
     communications_type = use_I2C;
     //Atlas_SoftSerial comms;
     Wire.begin(); // Concerns about loading this more than once in different
                   // libraries. See:
                   // https://github.com/esp8266/Arduino/issues/2607
-  };
+  }
   else{
     Serial.println("No valid measurement type chosen. Halting.");
-    while(1){};
+    while(1){}
     // Possibly add warning LED here
   }
+  //
+  //mySerial(softSerRX, softSerTX);
+
 }
 
 /*
@@ -200,7 +208,7 @@ AtlasNW::AtlasNW(char* _measurement_type, char* _communications_type, uint8_t I2
 // SENDING DATA //
 //////////////////
 
-void AtlasNW::I2C_write(char* _transmission, _transmission_length){
+void AtlasNW::I2C_write(char* _transmission, uint8_t _transmission_length){
   Wire.beginTransmission(I2C_address);
   Wire.write(_transmission, _transmission_length);
   Wire.write('\r');
@@ -218,6 +226,7 @@ void AtlasNW::Serial_write(char* _transmission){
 // Just use software serial for now, to get the ball rolling in time
 // to go to the field
 void AtlasNW::SoftSer_write(char* _transmission){
+  SoftwareSerial mySerial(softSerRX, softSerTX);
   mySerial.begin(baudRate);
   mySerial.print(_transmission);
   mySerial.print('\r');
@@ -243,12 +252,13 @@ void AtlasNW::LED_on(bool _state){
    */
   char* _transmission;
   uint8_t response_byte;
+  SoftwareSerial mySerial(softSerRX, softSerTX);
   mySerial.begin(baudRate);
   mySerial.print("L,");
   mySerial.print(_state);
   mySerial.print("\r");
   // dump the response
-  while mySerial.available(){
+  while (mySerial.available()){
     response_byte = mySerial.read();
   }
   mySerial.end();
@@ -261,12 +271,13 @@ void AtlasNW::response_code_on(bool _state){
    */
   char* _transmission;
   uint8_t response_byte;
+  SoftwareSerial mySerial(softSerRX, softSerTX);
   mySerial.begin(baudRate);
   mySerial.print("RESPONSE,");
   mySerial.print(_state);
   mySerial.print("\r");
   // dump the response
-  while mySerial.available(){
+  while (mySerial.available()){
     response_byte = mySerial.read();
   }
   mySerial.end();
@@ -278,12 +289,13 @@ void AtlasNW::continuous_readings_on(bool _state){
    */
   char* _transmission;
   uint8_t response_byte;
+  SoftwareSerial mySerial(softSerRX, softSerTX);
   mySerial.begin(baudRate);
   mySerial.print("C,");
   mySerial.print(_state);
   mySerial.print("\r");
   // dump the response
-  while mySerial.available(){
+  while (mySerial.available()){
     response_byte = mySerial.read();
   }
   mySerial.end();
@@ -295,12 +307,13 @@ void AtlasNW::set_K_constant(float K){
    */
   char* _transmission;
   uint8_t response_byte;
+  SoftwareSerial mySerial(softSerRX, softSerTX);
   mySerial.begin(baudRate);
   mySerial.print("K,");
   mySerial.print(K);
   mySerial.print("\r");
   // dump the response
-  while mySerial.available(){
+  while (mySerial.available()){
     response_byte = mySerial.read();
   }
   mySerial.end();
@@ -312,6 +325,7 @@ void AtlasNW::set_Temperature(float T){
    */
   char* _transmission;
   uint8_t response_byte;
+  SoftwareSerial mySerial(softSerRX, softSerTX);
   mySerial.begin(baudRate);
   mySerial.print("T,");
   mySerial.print(T);
@@ -319,26 +333,28 @@ void AtlasNW::set_Temperature(float T){
   // Time to process
   delay(300);
   // dump the response
-  while mySerial.available(){
+  while (mySerial.available()){
     response_byte = mySerial.read();
   }
   mySerial.end();
 }
 
-void AtlasNW::calibrate();
+void AtlasNW::calibrate(){
   /*
    Two-point calibration: low
    */
   char* _transmission;
+  uint8_t response_byte;
+  SoftwareSerial mySerial(softSerRX, softSerTX);
   mySerial.begin(baudRate);
   Serial.println("Clearing calibration data");
   mySerial.print("Cal,clear\r");
   // dump the response
-  while mySerial.available(){
+  while (mySerial.available()){
     response_byte = mySerial.read();
   }
   Serial.println("Now dry the probe.");
-  Serial.println("Enter any character and press <ENTER> when ready")
+  Serial.println("Enter any character and press <ENTER> when ready");
   Serial.println("to perform the dry calibration.");
   while(!Serial.available()); // Wait for input from user
   mySerial.print("Cal,dry\r");
@@ -347,12 +363,12 @@ void AtlasNW::calibrate();
   Serial.println("to begin low-point calibration");
   while(!Serial.available()); // Wait for input from user
   mySerial.print("Cal,low,");
-  while Serial.available(){
+  while (Serial.available()){
     mySerial.print(Serial.read());
   }
-  mySerial.print("\r")
+  mySerial.print("\r");
   // dump the response
-  while mySerial.available(){
+  while (mySerial.available()){
     response_byte = mySerial.read();
   }
   Serial.println("Low-point calibration done.");
@@ -364,12 +380,12 @@ void AtlasNW::calibrate();
   Serial.println("to begin high-point calibration");
   while(!Serial.available()); // Wait for input from user
   mySerial.print("Cal,high,");
-  while Serial.available(){
+  while (Serial.available()){
     mySerial.print(Serial.read());
   }
-  mySerial.print("\r")
+  mySerial.print("\r");
   // dump the response
-  while mySerial.available(){
+  while (mySerial.available()){
     response_byte = mySerial.read();
   }
   mySerial.end();
@@ -383,17 +399,18 @@ char* AtlasNW::read(bool _state){
   char* _transmission;
   char _tmpchar;
   char* response;
+  SoftwareSerial mySerial(softSerRX, softSerTX);
   mySerial.begin(baudRate);
   mySerial.print("R\n");
   // Record the response
   // Dump response code
-  while mySerial.available(){
+  while (mySerial.available()){
     _tmpchar = mySerial.read();
   }
   // But record actual response
   delay(1000);
   int i = 0;
-  while mySerial.available(){
+  while (mySerial.available()){
     _tmpchar = mySerial.read();
     // Don't write the carriage return
     while (_tmpchar != "\r"){
